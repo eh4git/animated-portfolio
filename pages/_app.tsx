5908b67cebe8f65e54874a540f16b9a87204fdc6
eh4git <ehirsch760@gmail.com>
2022-09-13 21:33:20 -0700

    initial project setup

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return  <div className='grid grid-cols-12 gap-6 my-14 px-48'>
+    <div className='col-span-3 bg-white rounded-2xl'>SideBar</div>
+    <div className='col-span-9 bg-white rounded-2xl'>
+      <Component {...pageProps} />
+    </div>
+    </div>
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (  
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-100">Goodbye World!</h1>
+    </div>
+  );
+}
+ 
+export default index;
