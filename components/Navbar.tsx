1a95795144cb1b9cc28cc71b063e7534bd462ea0
eh4git <ehirsch760@gmail.com>
2022-09-14 11:01:56 -0700

    navbar functional, w/ initial styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,25 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+
+const NavItem: FunctionComponent<{
+  activeItem: string;
+  setActiveItem: Function;
+  name: string;
+  route: string;
+}> = ({ activeItem, setActiveItem, name, route }) => {
+  return activeItem !== name ? (
+    <Link href={route}>
+      <a>
+        <span onClick={() => setActiveItem(name)} className="hover:text-green">
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
+
+// export default NavItem;
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState<string>("");
@@ -13,30 +32,29 @@ const Navbar = () => {
     if (pathname === "/resume") setActiveItem("Resume");
   }, []);
   return (
-    <div>
-      <span className="font-bold text-green">{activeItem}</span>
-      <div className="flex space-x-3 text-red-400 font-lg">
-        {activeItem !== "About" && (
-          <Link href="/">
-            <a>
-              <span onClick={() => setActiveItem("About")}>About</span>
-            </a>
-          </Link>
-        )}
-        {activeItem !== "Projects" && (
-          <Link href="/projects">
-            <a>
-              <span onClick={() => setActiveItem("Projects")}>Projects</span>
-            </a>
-          </Link>
-        )}
-        {activeItem !== "Resume" && (
-          <Link href="/resume">
-            <a>
-              <span onClick={() => setActiveItem("Resume")}>Resume</span>
-            </a>
-          </Link>
-        )}
+    <div className="flex justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 text-green border-green md:text-2xl">
+        {activeItem}
+      </span>
+      <div className="flex space-x-5 text-lg text-red-400 ">
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="About"
+          route="/"
+        />
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="Projects"
+          route="/projects"
+        />
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="Resume"
+          route="/resume"
+        />
       </div>
     </div>
   );
---
+++ data.ts
@@ -0,0 +1,21 @@
+import {RiComputerLine} from 'react-icons/ri';
+import { Service } from './type';
+
+
+export const services:Service[] = [
+    {
+        title: 'Full Stack Dev',
+        about: 'lorem ipsum....',
+        Icon: RiComputerLine
+    },
+    {
+        title: 'Full Stack Dev',
+        about: 'lorem ipsum....',
+        Icon: RiComputerLine
+    },
+    {
+        title: 'Full Stack Dev',
+        about: 'lorem ipsum....',
+        Icon: RiComputerLine
+    }
+]
