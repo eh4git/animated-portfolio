b30c29ed9ebb79e368342db65a6e69ade937c0ff
eh4git <ehirsch760@gmail.com>
2022-09-15 00:32:08 -0700

    about page initial setup and styling complete

--- data.ts
+++ data.ts
@@ -1,21 +1,22 @@
-import {RiComputerLine} from 'react-icons/ri';
+import { RiComputerLine, RiCloudLine } from 'react-icons/ri';
+import { IoEarth } from 'react-icons/io5'
 import { Service } from './type';
 
 
 export const services:Service[] = [
     {
-        title: 'Full Stack Dev',
-        about: 'lorem ipsum....',
+        title: 'Frontend Dev',
+        about: 'Incorporating component libraries such as <b>Bootstrap</b> and <b>MaterialUI</b>, or frameworks such as <b>Tailwind</b>, I can provide eye-catching and responsive UI.',
         Icon: RiComputerLine
     },
     {
-        title: 'Full Stack Dev',
-        about: 'lorem ipsum....',
-        Icon: RiComputerLine
+        title: 'Backend Dev',
+        about: 'Utilizing <b>Node.js</b> and <b>Express</b>, or <b>PHP</b>, I can create server-side applications to host a wide array of functionalities.',
+        Icon: RiCloudLine
     },
     {
         title: 'Full Stack Dev',
-        about: 'lorem ipsum....',
-        Icon: RiComputerLine
-    }
+        about: 'I can build beautiful full stack applications using <b>HTML</b>, <b>CSS</b>, <b>React</b>, and <b>MySQL</b> or <b>MongoDB</b>.',
+        Icon: IoEarth
+    },
 ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
+// import { GetStaticPropsContext } from "next";
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../data";
 const index = () => {
-  return (  
-    <div>
-      <h1 className="">Goodbye World!</h1>
+  console.log(services);
+  return (
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        I am an enthusiastic full stack web developer. I have worked with a
+        range of tech stacks including....
+      </h5>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400"
+        style={{ marginRight: "-1.5rem", marginLeft: "-1.5rem" }}
+      >
+        <h6 className="text-xl font-bold tracking-wide my-34">What I Offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
+          {services.map(service => (
+            <div className="bg-gray-200 rounded-lg lg:col-span-1">
+              <ServiceCard service={service} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
- 
+};
+
 export default index;
+
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const data = await fetch("http://localhost:3000/api/services");
+//   const services = await data.json();
+//   console.log("server", services);
+//   return {
+//     props: { services },
+//   };
+// };
