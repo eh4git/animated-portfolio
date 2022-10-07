2c46f817c7d26c1eb35f58d47e7c8ce981954fbc
eh4git <ehirsch760@gmail.com>
2022-10-06 21:55:22 -0700

    fixing useEffect Dep error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,13 +26,13 @@ const NavItem: FunctionComponent<{
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState<string>("");
-
   const { pathname } = useRouter();
 
   useEffect(() => {
     if (pathname === "/") setActiveItem("About");
     if (pathname === "/projects") setActiveItem("Projects");
     if (pathname === "/resume") setActiveItem("Resume");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="flex justify-between px-5 py-3 my-3">
